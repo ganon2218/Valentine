b9a5ceed16a7ad4c04140a649e318e0c50ed36ff
ganon2218 <nipurn2001@gmail.com>
2026-02-13 20:56:28 +0530

    First commit.

---
+++ js/diary.js
@@ -0,0 +1,188 @@
+const entries = [
+  {
+    image: "assets/images/couple/2-10-21.jpg",
+    date: "2 October, 2021",
+    location: "Juhu, Mumbai",
+    text: `Dear Diary,
+     I had been looking forward to this day since months now. Finally I got to meet Disha in person today.
+     We were meeting as friends but today for the first time I experienced what people call chemistry. There was this unsaid connection I felt with her which gave me goosebumps. It was not supposed to be a date but it really felt like one. And boy i felt like I could really have a great time with her even though her soul belongs to someone else. Well that usually is the least of my worries.  
+    
+     But somehow this felt like the start of something new and uncharted.`
+  },
+  {
+    image: "assets/images/couple/30-1-22.jpg",
+    date: "29 January, 2022",
+    location: "Delerium",
+    text: `Dear Diary,
+     This time I'm fucked for real. My Minor project group is functionally dead as HRC claims my members as their interns. Me and Disha are all that's left. Now I am left with little choices and all tough ones. But how will I tell Disha about this? It's her birthday tomorrow. Will she be heartbroken? Will she think that I am making a practical decision by officially disolving the group? Or will she think I betrayed her cause I am the bearer of this terrible news? I know for sure that, I will never betray her by joining another group.
+     
+     God give me strength to survive this. I really do not want to choose between her and career.`
+  },
+  {
+    image: "assets/images/couple/9-7-22.jpg",
+    date: "9 July, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     It's been a while since I have written here. The Pandemic ended, college reopeneded and I made a few new friends, lived life a little until everyone else returned a few days ago. Every now and then Disha crosses paths with me, we make a brief eye contact and then just pass each other. No words are exchanged though many unsaid. I just wish I knew how to get her back in my life. But how? My decision shattered her on her birthday, how do I even face her?
+       
+     It's been 6 months since we've talked. Even after everything she wished me on my birthday, and i've been guilt ridden since.`
+  },
+  {
+    image: "assets/images/couple/24-7-22.jpg",
+    date: "24 July, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     Today I'm a mess. Suyash spotted Disha from afar limping towards her hostel. She had blood stained bandaids around both her ankles. She was in extreme agony. I wanted to go up to her and just help her back to her place and tend to her wounds but I chickened out. She's a lioness wounded but yet determined. I just wish I had the courage to go up to her and just say something. 
+     A few days ago me and viv were sitting together in the OAT and she came up to us and talked to Viv even though she barely knew her. She did greet me but her mere presence dried my throat to the extent that no voice left my mouth. Enough is enough. I know I fucked up but I'm gonna text her.`
+  },
+  {
+    image: "assets/images/couple/2-8-22.jpg",
+    date: "2 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     I'm so glad I texted Disha. We went for a long walk even though she was injured. As expected the chemistry hit again. Boy, beneath her serious and professional nature she knows how to flirt. Today she gave me an empty juice bottle and told me to keep it. When I turned it, I realized that she had smeared her lipstick on it's side. NO ONE HAS EVER FLIRTED THIS HARD WITH ME. Usually I'm the one who makes girls around me blush but here she's giving me the butterflies. 
+     
+     Now i'm sitting here with this bottle wanting to kiss that imprint but too afraid that i'll ruin it.`
+  },
+  {
+    image: "assets/images/couple/5-8-22.jpg",
+    date: "5 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     She finally agreed to let me take her out on a date to this cute cafe near campus. That cafe was the perfect place to take her cause I saw a guitar hanging on their wall. I borrowed it and started playing it. That's when I saw it, she was looking away from me and could'nt stop smiling. Looks like it was time for revange on all the times she made me blush helplessly in public. I just looked right in her eyes and played a romantic song I knew pretty well. I could literally sense the girls in the booth next to me burn with jealousy.
+     
+     This is the first time I made her blush this hard. I'm so damn addicted to her.`
+  },
+  {
+    image: "assets/images/couple/6-8-22.jpg",
+    date: "6 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     Looks like yesterday's guitar worked on her much more than I expected it to. She asked me the question in the cutest way I could have imagined. "Is it okay if I wanna fall in love with you?", she asked. I was caught completely off guard.
+     But with that came another truth, that if something were to happen between us, it could only be for 10 months, post which she will leave India for 10 years to study and  find work in the US. What should I do? I really want her forever.
+          
+     I guess I need to consult this one with the boys and Viv. It's a toughie.`
+  },
+  {
+    image: "assets/images/couple/9-8-22.jpg",
+    date: "9 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     Today was a very emotionally challenging day for both of us. Our day started off with a long walk to the rocks of Shikhar Chandi. Things became quickly romantic when I got her down from there by holding her in my arms. Our lips were so close that i could have gone for the kiss, but it seemed her past was stopping her.
+     I nudged her to just kiss me to get past it. I felt so selfish, yet I tried to convincing myself that it was for her own good.
+     
+     We did'nt kiss but I guess giving her time to deal with her emotions is for the best.`
+  },
+  {
+    image: "assets/images/couple/10-8-22.jpg",
+    date: "10 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary, (we kissed).
+     We were chilling in the KSAC Stadium when it started pouring heavily. We got completely drenched by the time we got in. Since we were stuck there Disha asked if I wanted to share earphones. She played "Hips don't lie". 
+     Midway through the song I saw her looking at me, I just looked back and there was this awkward pause. Idk why, I just went for the kiss but then hesitated, but she just pulled me back and kissed me. She took my hand and put it on her breast while we were still at it. I pressed it and snogged her for what felt never-ending.
+     
+     It was the first kiss of my life and it just felt so wet and electrifying. I'm in heaven.`
+  },
+  {
+    image: "assets/images/couple/21-8-22.jpg",
+    date: "21 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     Life has been until this new problem arose. Disha's ex found out about us and landed here thinking he can win her back. Disha has been persistant on letting her meet him once if not alone, in my presence. I know that will end badly either way given his history.
+     Today I finally got my placement news and Disha called me to the main road and did'nt say a thing. I was quick to assume that her ex got to her and convinced her to let him meet me. I called up Sudhanshu and headed there with trembling hands and 10 of his guys assuming her ex is prepared. When I got there Disha gave me this bouquet and hugged me tight congratualating me.
+     I feel ashamed of myself.`
+  },
+  {
+    image: "assets/images/couple/31-8-22.jpg",
+    date: "31 August, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     Disha has such great friends. She introduced me to Roshni, her flat mates a few days back. Since then we have been hanging out almost daily.
+     I feel so lucky to have Disha. With her I have become a part of her circle as well. Her friends are nothing like mine. They are all sweet and true people.
+     
+     And ofcourse today is Saraswati Puja and today Disha was looking so absolutely stunning in her saree. I am just fighting back the urge of demanding Disha to wear Indian attire everyday.`
+  },
+  {
+    image: "assets/images/couple/11-9-22.jpg",
+    date: "11 September, 2022",
+    location: "Bhubaneswar, Odisha",
+    text: `Dear Diary,
+     Something happened between us. Something I absolutely am not going write about here. Something I am abosultely shy about discussing even with you.
+     All I can say is MANN Disha is so damn hot. And she has such well defined abs. 
+     
+     Tonight i'm exhausted, i'm electrified, i'm going crazy, I have no idea what all I'm feeling all at the same time. But I know one thing for sure and I told it to her. I am gonna marry her come what may.
+     And yes from today onwards it's official. 
+     
+     I have a girlfriend. And her name is Disha Saxena.`
+  },
+];
+
+let currentEntry = 0;
+
+function loadEntry() {
+  document.getElementById("memoryPhoto").src = entries[currentEntry].image;
+  document.getElementById("entryDate").innerText = entries[currentEntry].date;
+  document.getElementById("entryLocation").innerText = entries[currentEntry].location;
+  document.getElementById("entryText").innerText = entries[currentEntry].text;
+  document.getElementById("pageNumber").innerText =(currentEntry + 1) + " / " + entries.length;
+}
+
+function animateEntryChange(direction) {
+
+  const entry = document.getElementById("entryText");
+  const photo = document.getElementById("memoryPhoto");
+
+  entry.classList.add("flip-out");
+  photo.classList.add("blur-out");
+
+  setTimeout(() => {
+
+    if (direction === "next" && currentEntry < entries.length - 1) {
+      currentEntry++;
+    }
+
+    if (direction === "prev" && currentEntry > 0) {
+      currentEntry--;
+    }
+
+    loadEntry();
+
+    entry.classList.remove("flip-out");
+    entry.classList.add("flip-in");
+
+    photo.classList.remove("blur-out");
+    photo.classList.add("blur-in");
+
+    setTimeout(() => {
+      entry.classList.remove("flip-in");
+      photo.classList.remove("blur-in");
+    }, 350);
+
+  }, 350);
+}
+
+function nextEntry() {
+  if (currentEntry < entries.length - 1) {
+    animateEntryChange("next");
+  } else {
+    closeDiary();
+  }
+}
+
+function prevEntry() {
+  animateEntryChange("prev");
+}
+
+function closeDiary() {
+  const cover = document.getElementById("cover");
+  const spread = document.getElementById("spread");
+
+  spread.style.display = "none";
+  cover.style.display = "flex";
+
+  setTimeout(() => {
+    cover.classList.remove("open");
+  }, 50);
+}
+
+loadEntry();
